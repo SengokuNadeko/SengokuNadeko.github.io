5898939a6845ce47794c87d491bba4c62172ed7e
JihadBeydoun <hsun25874@gmail.com>
2023-05-21 18:46:25 -0400

    Overhauled, added a navbar for both pc and mobile view

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-// JavaScript code for website
+const menu = document.querySelector('#mobile_menu');
+const menu_link = document.querySelector('.navbar_menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is_active');
+    menu_link.classList.toggle('active');
+});
